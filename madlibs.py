97b268c3152c94d241616fa9a9fe25002d188a6b
Coronado1221 <ap21.coronado@gmail.com>
2026-01-22 12:18:35 -0600

    C:\Users\acoronado\.vscode\madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,27 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Assignment Name, Date, File Name).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+# Humpty Dumpty
+print("\n\n\nHumpty Dumpty")
+print("                by: Anthony Coronado")
+
+# Variables
+
+person_name = input("Person's Name:  ")
+noun = input("Noun:  ")
+verb = input("Verb:  ")
+adjective = input("Adjective:  ")
+
+# output
+
+print(f"{person_name} {verb} on a {noun}")
+print(f"{person_name} had a great {adjective}")
