2524872a32d93ad0fd4848946f2ee43c06855641
Coronado1221 <ap21.coronado@gmail.com>
2026-01-26 21:33:50 -0600

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,26 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+# Variables
+
+animal_name = "Killer Whale"     # String
+age = 12                         # Integer
+weight = 500.75                  # Float
+male = True                      # Boolean
+
+# Printed Variables 
+
+print (f"Animal:  {animal_name}")
+print (f"Age:     {age} years old")
+print (f"Weight:  {weight} lbs")
+print (f"Male:    {male}")
