7b9c255b5f75f691531541fb3058ca8dc38198c8
Anthony Coronado <ap21.coronado@gmail.com>
2026-02-12 12:48:51 -0600

    a

--- match.py
+++ match.py
@@ -2,14 +2,42 @@
 Match case
 
 """
-month = input("Enter a month name: ").lower()
 
-match month:
-    case "january" | "march" | "may" | "july" | "august" | "october" | "december":
-        print("There are 31 days in this month.")
-    case "april" | "june" | "september" | "november":
-        print("There are 30 days in this month.")
-    case "february":
-        print("There are 28 or 29 days in this month.")
-    case _:
-        print("That is not a valid month name.")
+# Display Menu
+print("1. Balance")
+print("2. Deposit")
+print("3. Withdraw")
+print("4. Transfer")
+print("5. Exit")
+
+Total = 1000
+
+while True:
+    choice = int(input("Please enter the number of your choice:  "))
+    if choice == 5:
+        print("Order complete")
+        print(f"Total: ${Total:,.2f}")
+        break
+    elif choice == 1:
+        print(f"Total: ${Total:,.2f}")
+    elif choice == 2:
+        deposit = int(input("Amount to deposit?  "))
+        print(f"Total: ${Total+deposit:,.2f}")
+    elif choice == 3:
+        withdraw = int(input("Amount to withdraw?"))
+        if withdraw > 1000:
+            else print ("Insufficient funds")
+            print(f"Total: ${Total-withdraw:,.2f}")
+             
+             
+
+
+    
+
+    
+          
+
+
+
+
+
