2b25715881645baa942949b2f37178aad9e025aa
Anthony Coronado <ap21.coronado@gmail.com>
2026-02-16 20:53:38 -0600

    reviewed

---
+++ match.py
@@ -0,0 +1,62 @@
+"""
+Match case
+
+"""
+
+# Display Menu
+print("1. Balance")
+print("2. Deposit")
+print("3. Withdraw")
+print("4. Transfer")
+print("5. Exit")
+
+Total = 1000
+
+while True:
+    choice = int(input("Please enter the number of your choice:  "))
+    if choice == 5:
+        print("Order complete")
+        print(f"Total: ${Total:,.2f}")
+        break
+    elif choice == 1:
+        print(f"Total: ${Total:,.2f}")
+    elif choice == 2:
+        deposit = int(input("Amount to deposit?  "))
+<<<<<<< HEAD
+        Total += deposit 
+        print(f"Total: ${Total:,.2f}")
+    elif choice == 3:
+        withdrawal_amount = float(input("How much to withdraw?  "))
+        if withdrawal_amount <= Total:
+            Total -= withdrawal_amount
+            print(f"Total: ${Total:,.2f}")
+        else: print("insufficient funds")
+    elif choice == 4:
+        transfer_amount = float(input("How much to transfer?  "))
+        if transfer_amount <= Total:
+            Total -= transfer_amount
+            print(f"Total: ${Total:,.2f}")
+        else: print("insufficient funds")
+    else:
+        print("I'm sorry that is not a valid menu number")
+=======
+        print(f"Total: ${Total+deposit:,.2f}")
+    elif choice == 3:
+        withdraw = int(input("Amount to withdraw?"))
+        if withdraw > 1000:
+            else print ("Insufficient funds")
+            print(f"Total: ${Total-withdraw:,.2f}")
+             
+             
+
+
+    
+
+    
+          
+
+
+
+
+
+>>>>>>> 7b9c255b5f75f691531541fb3058ca8dc38198c8
